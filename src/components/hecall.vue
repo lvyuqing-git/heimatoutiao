8112b01284b634008f1a13b9b6f0465c9347089c
一打砖头拍不倒 <lyq15360562781@163.com>
2020-01-08 23:49:34 +0800

    封装cell组件

---
+++ src/components/hecall.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="cell">
+		<div class="left">
+			<slot name="left"></slot>
+		</div>
+		<div class="right">
+			<slot name="right"> </slot>
+			<span>
+				<van-icon name="arrow" />
+			</span>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.cell {
+	display: flex;
+	justify-content: space-between;
+	border-bottom: 1px solid #ccc;
+	height: 50px;
+	line-height: 50px;
+    margin: 0 15px;
+    .right{
+        display: flex
+    }
+}
+</style>
