d13bbebb23d9c4362ad5d1375390a085f747b48e
一打砖头拍不倒 <lyq15360562781@163.com>
2020-01-09 00:32:11 +0800

    用户信息页面静态渲染完成

--- src/views/Edit_profile.vue
+++ src/views/Edit_profile.vue
@@ -1,83 +1,78 @@
 <template>
-	<div id="app">
-		<div class="editprofile">
-			<div data-v-3a9f86c9="" class="header">
-				<span data-v-3a9f86c9="" class="iconfont iconjiantou2"></span>
-				<h4 data-v-3a9f86c9="">编辑用户资料</h4>
-				<span data-v-3a9f86c9=""></span>
-			</div>
-			<div class="head">
-				<img
-					alt=""
-					src="http://localhost:3000/uploads/image/IMG1570927791226.jpeg"
-				/>
-				<div class="uploader van-uploader">
-					<div class="van-uploader__wrapper">
-						<div class="van-uploader__upload">
-							<i
-								class="van-icon van-icon-plus van-uploader__upload-icon"
-								><!----></i
-							><input
-								type="file"
-								accept="image/*"
-								class="van-uploader__input"
-							/>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div data-v-5619741e="" class="mycell">
-				<span data-v-5619741e="">昵称</span
-				><span data-v-5619741e="">itcast123</span
-				><span data-v-5619741e="" class="iconfont iconjiantou1"></span>
-			</div>
-			<!---->
-			<div data-v-5619741e="" class="mycell">
-				<span data-v-5619741e="">密码</span
-				><span data-v-5619741e="">******</span
-				><span data-v-5619741e="" class="iconfont iconjiantou1"></span>
-			</div>
-			<!---->
-			<div data-v-5619741e="" class="mycell">
-				<span data-v-5619741e="">性别</span
-				><span data-v-5619741e="">女</span
-				><span data-v-5619741e="" class="iconfont iconjiantou1"></span>
-			</div>
-			<!---->
-		</div>
-	</div>
+  <div id="app">
+    <div class="editprofile">
+      <div class="header">
+        <span class="iconfont iconjiantou2"></span>
+        <h4>编辑用户资料</h4>
+        <span></span>
+      </div>
+      <div class="head">
+        <img alt=""
+             src="http://127.0.0.1:3000/uploads/image/default.jpeg" />
+      </div>
+    
+    <hmcell>
+        <div slot="left">
+            昵称
+        </div>
+        <div slot="right">
+            火星网友
+        </div>
+    </hmcell>
+       <hmcell>
+        <div slot="left">
+            密码
+        </div>
+        <div slot="right">
+            火星网友
+        </div>
+    </hmcell>
+       <hmcell>
+        <div slot="left">
+            性别
+        </div>
+        <div slot="right">
+            火星网友
+        </div>
+    </hmcell>
+
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import hmcell from '../components/hecall'
+export default {
+    components: {
+        hmcell
+    }
+}
 </script>
 
 <style lang="less" scoped>
-.head {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 20px;
-  position: relative;
-
-  .uploader {
-    position: absolute;
-    opacity: 0;
+.editprofile {
+  margin: 0 15px;
+  .header {
+    display: flex;
+    justify-content: space-between;
+    height: 30px;
+    line-height: 30px;
+    h4 {
+      font-weight: bold;
+      color: #000;
+    }
   }
-
-  img {
-    display: block;
-    width: 100 / 360 * 100vw;
-    height: 100 / 360 * 100vw;
-    border-radius: 50%;
-  }
-  /deep/.van-uploader__upload {
-    width: 100 / 360 * 100vw;
-    height: 100 / 360 * 100vw;
-  }
-  /deep/.van-picker-column__item{
-      border-top:1px red sloid;
-      border-bottom:1px red sloid;
+  .head {
+    position: relative;
+    height: 100px;
+    margin: 20px 0;
+    img {
+      position: absolute;
+      border-radius: 50%;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%,-50%)
+    }
   }
 }
 </style>
